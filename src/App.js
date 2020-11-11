3f290ec0d84e36196897f26e9e330cc8bffae4ad
milanjacimovic <jacimovicmilanfon@gmail.com>
2020-11-11 16:33:23 +0100

    Added basic router navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const FreshPage = () => (
+  <div>
+    <h1>FRESH</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/fresh" component={FreshPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,18 +14,21 @@ class Directory extends React.Component {
           title: "fresh",
           imageUrl: "https://media.finishline.com/i/finishline/CJ6700_100_P1",
           id: 1,
+          linkUrl: "fresh",
         },
         {
           title: "popular",
           imageUrl:
             "https://c.static-nike.com/a/images/t_PDP_1280_v1/f_auto/gsuin11ptg5qgktmzoat/air-force-1-07-mens-shoe-JkTGzADv.jpg",
           id: 2,
+          linkUrl: "popular",
         },
         {
           title: "cheap",
           imageUrl:
             "https://c.static-nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/vgku2dk60hh1zgnnr4o1/tanjun-big-kids-shoe-tmF5j2.jpg",
           id: 3,
+          linkUrl: "cheap",
         },
         {
           title: "womens",
@@ -33,6 +36,7 @@ class Directory extends React.Component {
             "https://cdn.shopify.com/s/files/1/0256/4243/4606/products/product-image-1197926037_1200x1200.jpg?v=1575443956",
           size: "large",
           id: 4,
+          linkUrl: "womens",
         },
         {
           title: "mens",
@@ -40,6 +44,7 @@ class Directory extends React.Component {
             "https://chekich.com/i/l/000/0001613_chekich-ch253-st-black-men-sneakers.jpeg",
           size: "large",
           id: 5,
+          linkUrl: "mens",
         },
       ],
     };
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
