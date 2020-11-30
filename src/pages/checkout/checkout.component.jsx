b38a65e4c56ffe4409bf42d9699b4d329b84bb5a
milanjacimovic <jacimovicmilanfon@gmail.com>
2020-11-30 22:23:51 +0100

    Added checkout item component and updated cart to use selectors

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import {
   selectCartItems,
   selectCartTotal,
@@ -12,26 +14,28 @@ import "./checkout.styles.scss";
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Product</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Description</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Quantity</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Price</span>
       </div>
-      <div className="header-blocks">
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => cartItem.name)}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
 
     <div className="total">
-      <span>TOTAL: {total}</span>
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
